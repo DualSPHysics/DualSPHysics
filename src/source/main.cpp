7fc29ba1e22bd6ef5e41edcbe55f556a63e69872
jmdominguez <jmdalonso@gmail.com>
2018-10-19 23:32:19 +0200

    Updates DSPHChrono library for any CPU on Windows.

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -55,7 +55,7 @@ Please download source files and documentation from <a href="http://dual.sphysic
 using namespace std;
 
 
-JAppInfo AppInfo("DualSPHysics4","v4.3.003","16-10-2018");
+JAppInfo AppInfo("DualSPHysics4","v4.3.004","19-10-2018");
 //JAppInfo AppInfo("DualSPHysics4","v4.2.???","UserVersion","v1.0","??-??-????"); //-for user versions.
 
 //==============================================================================
