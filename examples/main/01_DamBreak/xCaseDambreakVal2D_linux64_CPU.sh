1af17191b137402032f906fb6d6aa290619e7fba
jmdominguez <jmdalonso@gmail.com>
2020-07-23 16:45:08 +0200

    Updates examples folder according to v5.0

--- examples/main/01_DamBreak/xCaseDambreakVal2D_linux64_CPU.sh
+++ examples/main/01_DamBreak/xCaseDambreakVal2D_linux64_CPU.sh
@@ -1,10 +1,10 @@
-  #!/bin/bash 
-  
-  fail () { 
-    echo Execution aborted. 
-    read -n1 -r -p "Press any key to continue..." key 
-    exit 1 
-  }
+#!/bin/bash 
+
+fail () { 
+ echo Execution aborted. 
+ read -n1 -r -p "Press any key to continue..." key 
+ exit 1 
+}
 
 # "name" and "dirout" are named according to the testcase
 
@@ -16,17 +16,17 @@ export diroutdata=${dirout}/data
 
 export dirbin=../../../bin/linux
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${dirbin}
-export gencase="${dirbin}/GenCase4_linux64"
-export dualsphysicscpu="${dirbin}/DualSPHysics4.4CPU_linux64"
-export dualsphysicsgpu="${dirbin}/DualSPHysics4.4_linux64"
-export boundaryvtk="${dirbin}/BoundaryVTK4_linux64"
-export partvtk="${dirbin}/PartVTK4_linux64"
-export partvtkout="${dirbin}/PartVTKOut4_linux64"
-export measuretool="${dirbin}/MeasureTool4_linux64"
-export computeforces="${dirbin}/ComputeForces4_linux64"
-export isosurface="${dirbin}/IsoSurface4_linux64"
-export flowtool="${dirbin}/FlowTool4_linux64"
-export floatinginfo="${dirbin}/FloatingInfo4_linux64"
+export gencase="${dirbin}/GenCase_linux64"
+export dualsphysicscpu="${dirbin}/DualSPHysics5.0CPU_linux64"
+export dualsphysicsgpu="${dirbin}/DualSPHysics5.0_linux64"
+export boundaryvtk="${dirbin}/BoundaryVTK_linux64"
+export partvtk="${dirbin}/PartVTK_linux64"
+export partvtkout="${dirbin}/PartVTKOut_linux64"
+export measuretool="${dirbin}/MeasureTool_linux64"
+export computeforces="${dirbin}/ComputeForces_linux64"
+export isosurface="${dirbin}/IsoSurface_linux64"
+export flowtool="${dirbin}/FlowTool_linux64"
+export floatinginfo="${dirbin}/FloatingInfo_linux64"
 
 option=-1
  if [ -e $dirout ]; then
@@ -50,7 +50,7 @@ if [ -e ${dirout} ]; then rm -r ${dirout}; fi
 
 # CODES are executed according the selected parameters of execution in this testcase
 
-# Executes GenCase4 to create initial files for simulation.
+# Executes GenCase to create initial files for simulation.
 ${gencase} ${name}_Def ${dirout}/${name} -save:all
 if [ $? -ne 0 ] ; then fail; fi
 
@@ -60,8 +60,8 @@ if [ $? -ne 0 ] ; then fail; fi
 
 fi
 
-if [ $option -eq 2 ]; then
-# Executes PartVTK4 to create VTK files with particles.
+if [ $option -eq 2 -o $option -eq 1 ]; then
+# Executes PartVTK to create VTK files with particles.
 export dirout2=${dirout}/particles
 ${partvtk} -dirin ${diroutdata} -savevtk ${dirout2}/PartFluid -onlytype:-all,fluid -vars:+idp,+vel,+rhop,+press,+vor
 if [ $? -ne 0 ] ; then fail; fi
@@ -69,11 +69,11 @@ if [ $? -ne 0 ] ; then fail; fi
 ${partvtk} -dirin ${diroutdata} -savevtk ${dirout2}/PartBound -onlytype:-all,bound -vars:-all -last:0
 if [ $? -ne 0 ] ; then fail; fi
 
-# Executes PartVTKOut4 to create VTK files with excluded particles.
+# Executes PartVTKOut to create VTK files with excluded particles.
 ${partvtkout} -dirin ${diroutdata} -savevtk ${dirout2}/PartFluidOut -SaveResume ${dirout2}/_ResumeFluidOut
 if [ $? -ne 0 ] ; then fail; fi
 
-# Executes IsoSurface4 to create VTK files with slices of surface.
+# Executes IsoSurface to create VTK files with slices of surface.
 export dirout2=${dirout}/surface
 ${isosurface} -dirin ${diroutdata} -saveslice ${dirout2}/Slices 
 if [ $? -ne 0 ] ; then fail; fi
