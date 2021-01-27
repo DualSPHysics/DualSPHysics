a4d6bfc752bd1f3af147a30c73c04f69d89db0a7
jmdominguez <jmdalonso@gmail.com>
2021-01-27 17:46:02 +0100

    Fixed bug on inlet when zsurf is undefined on Linux and other minor improvements.

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -31,8 +31,8 @@ School of Mechanical, Aerospace and Civil Engineering, University of Manchester,
 \section compile_sec Project files
 Please download source files and documentation from <a href="http://dual.sphysics.org">DualSPHysics website.</a> \n
 \author <a href="http://dual.sphysics.org/index.php/developers">DualSPHysics Developers.</a> 
-\version 5.0.166
-\date 06-12-2020
+\version 5.0.168
+\date 27-01-2021
 \copyright GNU Lesser General Public License <a href="http://www.gnu.org/licenses/">GNU licenses.</a>
 */
 
@@ -56,7 +56,7 @@ Please download source files and documentation from <a href="http://dual.sphysic
 
 using namespace std;
 
-JAppInfo AppInfo("DualSPHysics5","v5.0.166","06-12-2020");
+JAppInfo AppInfo("DualSPHysics5","v5.0.168","27-01-2021");
 //JAppInfo AppInfo("DualSPHysics5","v5.0.???","UserVersion","v1.0","??-??-????"); //-for user versions.
 
 //==============================================================================
