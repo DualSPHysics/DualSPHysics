bf159f399442f5b0a5cf0dc5db53e7835ef68b17
Iván Martínez Estévez <i.martinezestevez@gmail.com>
2022-07-06 13:06:30 +0200

    FSI: Include the folder examples/flexstructure

--- chpermissions.sh
+++ chpermissions.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 chmod +x bin/linux/*_linux64
+
 chmod +x examples/main/*/*.sh
 dos2unix examples/main/*/*.sh
-
+chmod +x examples/flexstructure/*/*.sh
+dos2unix examples/flexstructure/*/*.sh
 
 
 
