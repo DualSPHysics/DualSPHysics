181fda9a7c39f5f26260205ca0d362d88f7adb15
Iván Martínez Estévez <i.martinezestevez@gmail.com>
2022-07-07 11:35:15 +0200

    FSI: add scripts to execute the CaseTurekHronCSM3

---
+++ examples/flexstructure/02_TurekHron/wCaseTurekHronCSM3_win64_GPU.bat
@@ -0,0 +1,72 @@
+@echo off
+setlocal EnableDelayedExpansion
+rem Don't remove the two jump line after than the next line [set NL=^]
+set NL=^
+
+
+rem "name" and "dirout" are named according to the testcase
+
+set name=CaseTurekHronCSM3
+set dirout=%name%_out
+set diroutdata=%dirout%\data
+
+rem "executables" are renamed and called from their directory
+
+set dirbin=../../../bin/windows
+set gencase="%dirbin%/GenCase_win64.exe"
+set dualsphysicscpu="%dirbin%/DualSPHysics5.0CPU_win64.exe"
+set dualsphysicsgpu="%dirbin%/DualSPHysics5.0_win64.exe"
+set boundaryvtk="%dirbin%/BoundaryVTK_win64.exe"
+set partvtk="%dirbin%/PartVTK_win64.exe"
+set partvtkout="%dirbin%/PartVTKOut_win64.exe"
+set measuretool="%dirbin%/MeasureTool_win64.exe"
+set computeforces="%dirbin%/ComputeForces_win64.exe"
+set isosurface="%dirbin%/IsoSurface_win64.exe"
+set flowtool="%dirbin%/FlowTool_win64.exe"
+set floatinginfo="%dirbin%/FloatingInfo_win64.exe"
+
+:menu
+if exist %dirout% ( 
+	set /p option="The folder "%dirout%" already exists. Choose an option.!NL!  [1]- Delete it and continue.!NL!  [2]- Execute post-processing.!NL!  [3]- Abort and exit.!NL!"
+	if "!option!" == "1" goto run else (
+		if "!option!" == "2" goto postprocessing else (
+			if "!option!" == "3" goto fail else ( 
+				goto menu
+			)
+		)
+	)
+)
+
+:run
+rem "dirout" to store results is removed if it already exists
+if exist %dirout% rd /s /q %dirout%
+
+rem CODES are executed according the selected parameters of execution in this testcase
+
+rem Executes GenCase to create initial files for simulation.
+%gencase% %name%_Def %dirout%/%name% -save:all
+if not "%ERRORLEVEL%" == "0" goto fail
+
+rem Executes DualSPHysics to simulate SPH method.
+%dualsphysicsgpu% -gpu %dirout%/%name% %dirout% -dirdataout data -svres
+if not "%ERRORLEVEL%" == "0" goto fail
+
+:postprocessing
+rem Executes PartVTK to create VTK files with particles.
+set dirout2=%dirout%\particles
+%partvtk% -dirin %diroutdata% -savevtk %dirout2%/PartFluid -onlytype:-all,+fluid
+if not "%ERRORLEVEL%" == "0" goto fail
+
+%partvtk% -dirin %diroutdata% -savevtk %dirout2%/PartStructure -onlymk:13
+if not "%ERRORLEVEL%" == "0" goto fail
+
+
+:success
+echo All done
+goto end
+
+:fail
+echo Execution aborted.
+
+:end
+pause
