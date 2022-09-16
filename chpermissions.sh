5aeaa8ce64f6209d4cf3f3284b409f0cd59bc8d4
Joe O'Connor <joseph.oconnor@imperial.ac.uk>
2022-09-16 10:56:16 +0100

    Modified name of examples folder to flexstruc to match class name.

--- chpermissions.sh
+++ chpermissions.sh
@@ -4,8 +4,8 @@ chmod +x bin/linux/*_linux64
 
 chmod +x examples/main/*/*.sh
 dos2unix examples/main/*/*.sh
-chmod +x examples/flexstructure/*/*.sh
-dos2unix examples/flexstructure/*/*.sh
+chmod +x examples/flexstruc/*/*.sh
+dos2unix examples/flexstruc/*/*.sh
 
 
 
