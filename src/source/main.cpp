dd17c6f16bcf3f31d0bbec6f18125553379a72da
jmdominguez <jmdalonso@gmail.com>
2022-09-23 11:42:27 +0200

    Minor changes to compile without libraries.

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -31,8 +31,8 @@ School of Mechanical, Aerospace and Civil Engineering, University of Manchester,
 \section compile_sec Project files
 Please download source files and documentation from <a href="http://dual.sphysics.org">DualSPHysics website.</a> \n
 \author <a href="http://dual.sphysics.org/index.php/developers">DualSPHysics Developers.</a> 
-\version 5.2.250 beta
-\date 21-09-2022
+\version 5.2.251
+\date 23-09-2022
 \copyright GNU Lesser General Public License <a href="http://www.gnu.org/licenses/">GNU licenses.</a>
 */
 
@@ -56,7 +56,7 @@ Please download source files and documentation from <a href="http://dual.sphysic
 
 using namespace std;
 
-JAppInfo AppInfo("DualSPHysics5","v5.2.250 beta","21-09-2022");
+JAppInfo AppInfo("DualSPHysics5","v5.2.251 beta","23-09-2022");
 //JAppInfo AppInfo("DualSPHysics5","v5.0.???","UserVersion","v1.0","??-??-????"); //-for user versions.
 
 //==============================================================================
