23e7b35bb491724b946a409a32187a360eed782b
jmdominguez <jmdalonso@gmail.com>
2024-11-09 14:50:36 +0100

    Updates VS project files and restores JDsInitialize.cpp

--- src/source/JDsInitialize.cpp
+++ src/source/JDsInitialize.cpp
@@ -214,21 +214,6 @@ void JDsInitializeOp_FluidVel::Run(unsigned np,unsigned npb,const tdouble3* pos
     velrho[p].x=dir.x*v1;
     velrho[p].y=dir.y*v1;
     velrho[p].z=dir.z*v1;
-    velrho[p].x=pos[p].z;
-    velrho[p].z=-pos[p].x;
-    const double pi=3.141592653589793;
-    double press=0.0;
-    // 	  velrho[p].x=cos(2*pi*pos[p].x)*sin(2*pi*pos[p].z);
-    // 	  velrho[p].z=-sin(2*pi*pos[p].x)*cos(2*pi*pos[p].z);
-    // 	  double press=2-(1.0/4.0)*(cos(2*2*pi*pos[p].x)+cos(2*2*pi*pos[p].z));
-    // 	  velrho[p].w= pow(press/100+1,1.0/1.0);
-    // velrho[p].w=1+press/(10*10);
-    for(int n=1;n<6; n+=2){
-      for(int m=1;m<6; m+=2){
-          press+=(-32.0)/(m*n*pi*pi*((m*pi)*(m*pi)+(n*pi)*(n*pi)))*sin(m*pi*(pos[p].x+0.5))*sin(n*pi*(pos[p].z+0.5));
-      }
-    }
-        velrho[p].w=1+press/(100);
   }
 }
 
