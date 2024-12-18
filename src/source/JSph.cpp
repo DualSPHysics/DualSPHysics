db52faf9381fe01f7490a4a6ba8e03da8c1e3b34
Joe O'Connor <j.oconnor@epcc.ed.ac.uk>
2024-12-18 18:04:19 +0000

    FlexStruc: Removes symmetry check in flexstruc setup.

--- src/source/JSph.cpp
+++ src/source/JSph.cpp
@@ -1276,7 +1276,6 @@ void JSph::LoadCaseConfig(const JSphCfgRun* cfg){
   if(FlexStruc){
     if(PartBegin)   Run_Exceptioon("Simulation restart not allowed when FlexStruc is used.");
     if(PeriActive)  Run_Exceptioon("Flexible structures is not allowed with periodic conditions.");
-    if(Symmetry)    Run_Exceptioon("Flexible structures is not allowed with symmetry.");
   }
   //<vs_flexstruc_end>
 
